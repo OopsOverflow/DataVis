7d5e135191e0f83ef95abd90d4ac6eda911d78bc
OopsOverflow <houssemmahmoud18@gmail.com>
2023-02-06 21:22:51 +0100

    Configure the project using D3 this time and React.js

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { GroupedBarChart } from './charts/GroupedBarChart';
+import { type IGroupedData } from './types';
+
+const GROUPED_BAR_CHART_DATA: IGroupedData[] = [
+  { label: 'Apples', values: [60, 80, 100] },
+  { label: 'Bananas', values: [160, 200, 120] },
+  { label: 'Oranges', values: [60, 40, 10] },
+];
+function App(): React.ReactElement {
+  return (
+    <div>
+      <h1
+        style={{
+          textAlign: 'center',
+          fontSize: '2rem',
+          fontWeight: 'bold',
+          color: 'white',
+          marginBottom: 10,
+        }}
+      >
+        Re-Establishing the USSR to please Karl Marx
+      </h1>
+      <GroupedBarChart data={GROUPED_BAR_CHART_DATA} />
+      <p>Hover over the bars to see the values</p>
+    </div>
+  );
+}
+
+export default App;
