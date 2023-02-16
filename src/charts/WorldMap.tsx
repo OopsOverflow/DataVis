8445db2f09c79d5becba16cfdeda66ed685be918
OopsOverflow <houssemmahmoud18@gmail.com>
2023-02-16 06:50:51 +0100

    fix build errors

--- src/charts/WorldMap.tsx
+++ src/charts/WorldMap.tsx
@@ -1,34 +1,31 @@
-import React, {
-  type ReactElement,
-  useEffect,
-  useState,
-} from 'react';
-import { geoMercator, geoPath } from "d3-geo";
-import { feature } from "topojson-client";
-
+import React, { type ReactElement, useEffect, useState } from 'react';
+import { geoMercator, geoPath } from 'd3-geo';
+import { feature } from 'topojson-client';
 
 export function WorldMap(): ReactElement<SVGSVGElement> {
-
   const [worldData, setWorldData] = useState([]);
   const projection = () => geoMercator().scale(127).translate([400, 300]);
   const [selected, setSelect] = useState<any[]>(['GBR', 'USA', 'FRA', 'CHN']);
 
   useEffect(() => {
-
-    fetch("https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries-sans-antarctica.json")
-      .then((response : any) => {
+    fetch(
+      'https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries-sans-antarctica.json',
+    )
+      .then((response: any) => {
         if (response.status !== 200) {
           console.log(`There was a problem: ${response.status}`);
           return;
         }
-        return response.json()
-      }).then((data : any) => {
+        return response.json();
+      })
+      .then((data: any) => {
         // console.log(data)
-        setWorldData(feature(data, data.objects.countries1).features);
-      }).catch(err => { console.log(err); });
-
+        setWorldData((feature(data, data.objects.countries1) as any).features);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }, []);
-
 
   return (
     <div>
@@ -40,13 +37,14 @@ export function WorldMap(): ReactElement<SVGSVGElement> {
             return (
               <path
                 key={`path-${i}`}
-                d={geoPath().projection(projection())(d)}
-                fill={selected.includes(id) ? 'black' : "white"}
+                d={geoPath().projection(projection())(d) ?? 'undefined'}
+                fill={selected.includes(id) ? 'black' : 'white'}
                 stroke={'red'}
                 strokeWidth={1}
                 onClick={() => {
-                  if(selected.includes(id)) setSelect(selected.filter((s : any) => s !== id))
-                  else setSelect([...selected, id])
+                  if (selected.includes(id))
+                    setSelect(selected.filter((s: any) => s !== id));
+                  else setSelect([...selected, id]);
                 }}
               />
             );
@@ -55,4 +53,4 @@ export function WorldMap(): ReactElement<SVGSVGElement> {
       </svg>
     </div>
   );
-};
+}
--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,20 +1,26 @@
 import * as codename from '../../public/data/codename.json';
 
 const getCodename = (cname: string) => {
-  const { Entity, Code } = codename;
-  const index = Object.keys(Entity).find((key : any) => Entity[key] === cname);
+  const { Entity, Code } = codename as unknown as {
+    Entity: string[];
+    Code: string[];
+  };
+  const index = Object.keys(Entity).find(
+    (key: any) => Entity[key] === cname,
+  ) as unknown as number;
   const code = Code[index];
-  return code
-}
-
+  return code;
+};
 
 const getCountryName = (cname: string) => {
-  const { Entity, Code } = codename;
-  const index = Object.keys(Code).find((key : any) => Code[key] === cname);
+  const { Entity, Code } = codename as unknown as {
+    Entity: string[];
+    Code: string[];
+  };
+  const index = Object.keys(Code).find(
+    (key: any) => Code[key] === cname,
+  ) as unknown as number;
   const name = Entity[index];
-  return name
-}
-export {
-  getCodename,
-  getCountryName
-}
+  return name;
+};
+export { getCodename, getCountryName };
